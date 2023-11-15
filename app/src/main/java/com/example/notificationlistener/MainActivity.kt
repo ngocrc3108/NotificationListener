b3fd52f56ac7354a85f0d37b3ab485ebe7de4d30
ngocrc <tienngoc2003dh@gmail.com>
2023-11-15 23:51:15 +0700

    okhttp work!

--- app/src/main/java/com/example/notificationlistener/MainActivity.kt
+++ app/src/main/java/com/example/notificationlistener/MainActivity.kt
@@ -9,10 +9,21 @@ import android.content.IntentFilter
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
+import java.util.Objects
+
 
 class MainActivity : Activity() {
     private var txtView: TextView? = null
     private var nReceiver: NotificationReceiver? = null
+    //https://api.telegram.org/bot6170746237:AAGRau8vFZ_X3KMJHBMBygwJwZvJJ_3tDW0/sendMessage?chat_id=5318496948&text=Hello
+    private val appUrl = "https://api.telegram.org/bot6170746237:AAGRau8vFZ_X3KMJHBMBygwJwZvJJ_3tDW0/sendMessage?chat_id=5318496948&text=Hello"
+    var client = OkHttpClient()
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +34,7 @@ class MainActivity : Activity() {
         val filter = IntentFilter()
         filter.addAction("CatchNotification")
         registerReceiver(nReceiver, filter)
+
     }
 
     override fun onDestroy() {
@@ -35,6 +47,19 @@ class MainActivity : Activity() {
             val textData = intent.getStringExtra("text")
             val titleData = intent.getStringExtra("title")
             txtView!!.text = "$titleData: $textData \n${txtView!!.text}"
+
+            val request = Request.Builder()
+                .url(appUrl)
+                .build()
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    //txtView!!.text = "onFailure \n${txtView!!.text}"
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+                }
+            })
         }
     }
 }
